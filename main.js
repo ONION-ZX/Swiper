6dc57ddfaa3a5d4157ac90809f9c4b3753cf356e
ONION-ZX <zxonion@gmail.com>
2019-01-24 22:49:45 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,78 @@
+const slider = document.querySelector('.slider');
+const slides = slider.querySelectorAll('.item');
+let currentIndex = 0;
+let lastIndex = slides.length - 1;
+let defaultConfig = {
+    mode: 'slide',
+    interval: 1000
+}
+
+swiper();
+
+function swiper(config) {
+    slide();
+    let mergedConfig = Object.assign({}, defaultConfig, config);
+    setInterval(() => {
+        increment();
+        switch (mergedConfig.mode) {
+            case 'slide':
+                slide();
+                break;
+            case 'fade':
+                fade();
+                break;
+        }
+    }, mergedConfig.interval);
+}
+
+function slide() {
+    slideX();
+    slideY();
+}
+
+function slideX() {
+    let prev = getPrev();
+    let current = getCurrent();
+    let next = getNext();  
+
+    prev.style.left = -prev.offsetWidth + 'px';
+    current.style.left = 0;
+    next.style.left = next.offsetWidth + 'px';
+}
+
+function slideY() {
+    let prev = getPrev();
+    let current = getCurrent();
+    let next = getNext();  
+
+    prev.style.zIndex = 1;
+    current.style.zIndex = 2;
+    next.style.zIndex = 0;
+}
+
+function getPrev() {
+    let prevIndex;
+    currentIndex < 1 ? prevIndex = lastIndex : prevIndex = currentIndex - 1;
+    return slides[prevIndex];
+}
+
+function getCurrent() {
+    return slides[currentIndex];
+}
+
+function getNext() {
+    let nextIndex;
+    currentIndex >= lastIndex ? nextIndex = 0 : nextIndex = currentIndex + 1;
+    return slides[nextIndex];
+}
+
+function fade() {
+    console.log(1);
+}
+
+function increment() {
+    if(currentIndex >= lastIndex)
+        currentIndex = 0;
+    else   
+        currentIndex++;
+}
